60cfe4bce67b9f7f24cd4a42760e053be47b6a86
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-03-25 17:20:20 +0200

    work in progress

--- connection.py
+++ connection.py
@@ -1,6 +1,8 @@
-#! /Users/piotrjankiewicz/code/connector/env python3
+#! /Users/piotrjankiewicz/code/connector/env/bin/python3.6
+
 
 import mysql.connector
+from PrettyTable import PrettyTable
 
 config = {
     'user': 'root',
@@ -15,12 +17,17 @@ if __name__ == '__main__':
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
 
-    query = ('select * from user')
-
+    query = ('select FirstName from user')
     cursor.execute(query)
 
+    t = PrettyTable(['Name', 'Age'])
+    t.add_row(['Bob', 19])
+
+
     for (row) in cursor:
-        print("%s\n"%row.FirstName)
+        t.add_row([row])
+
+    print(t)
 
     cursor.close()
     cnx.close()
